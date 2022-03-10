28292904b0c32e006b75c739a4cb4379edc3eeb8
Gustavo Moreschi <guga@Gustavos-MacBook-Pro.local>
2022-03-10 19:35:01 -0300

    studentsigninpage

---
+++ src/pages/signinstudent/index.page.tsx
@@ -0,0 +1,23 @@
+import { ButtonKnewave } from "../../components/ButtonKnewave";
+import * as S from "./styles";
+
+export default function signinstudent() {
+    return (
+        <S.Wrapper>
+            <S.ContainerImage>
+            <S.Image src="/images/signinstudent.png" />
+            </S.ContainerImage>
+
+            <S.ContainerInformation>
+            <a href="#">Voltar</a>
+            <h1>Preencha os Campos</h1>
+            <p>Vamos enviar um e-mail para você, para confirmar a sua identidade.</p>
+            <ButtonKnewave variant="PRIMARY" size="sm" >Próximo</ButtonKnewave>
+
+            </S.ContainerInformation>
+
+
+
+        </S.Wrapper>
+    )
+    }
---
+++ src/pages/signinstudent/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+
+
+`
+export const Wrapper = styled.div`
+display: flex;
+`
+export const ContainerImage = styled.div`
+
+`
+export const ContainerInformation = styled.div`
+a{
+    text-decoration: none;
+    color: var(--blue);
+}
+padding: 47px 120px 26px 69px;
+`
