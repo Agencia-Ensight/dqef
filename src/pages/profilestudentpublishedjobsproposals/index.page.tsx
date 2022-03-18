169a471c00049cd3578940573dc6d024cbe0ecd0
Gustavo Moreschi <guga@Gustavos-MacBook-Pro.local>
2022-03-18 19:03:16 -0300

    newpage

---
+++ src/pages/profilestudentpublishedjobsproposals/index.page.tsx
@@ -0,0 +1,38 @@
+import { ButtonKnewave } from "../../components/ButtonKnewave";
+
+import * as S from "./styles";
+
+import { RiLockPasswordFill } from "react-icons/ri";
+
+import { UrgentWorkCard } from "../../components/UrgentWorkCard";
+
+import { ProfileCard } from "../../components/ProfileCard";
+
+import { ProfileCardEmployee } from "../../components/ProfileCardEmployee";
+
+
+export default function profilestudentpublishedjobsproposals() {
+    return (
+        <S.Wrapper>
+            <div>
+                <a href="#">Voltar</a>
+                <h1>Propostas Recebidas</h1>
+            <S.ContainerJobCard>
+            <S.ContainerProfileCard>
+            <ProfileCardEmployee img="/images/fotobraba.png" price={150} course="Direito" profileName="Bruno Vencato" jobs={25} studying="Cursando Oitavo Período" />
+            </S.ContainerProfileCard>
+            <S.ContainerProfileCard>
+            <ProfileCardEmployee img="/images/fotobraba.png" price={150} course="Direito" profileName="Bruno Vencato" jobs={25} studying="Cursando Oitavo Período" />
+            </S.ContainerProfileCard>
+            <S.ContainerProfileCard>
+            <ProfileCardEmployee img="/images/fotobraba.png" price={150} course="Direito" profileName="Bruno Vencato" jobs={25} studying="Cursando Oitavo Período" />
+            </S.ContainerProfileCard>
+            </S.ContainerJobCard>
+            </div>
+
+            
+
+
+        </S.Wrapper>
+    )
+    }
---
+++ src/pages/profilestudentpublishedjobsproposals/styles.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+
+
+`
+export const Wrapper = styled.div`
+display: flex;
+gap: 38px;
+margin: 50px 90px 72px 120px;
+
+
+
+h4{
+    font-size: 36px;
+    font-weight: 500;
+    color: black;
+    margin-bottom: 41px;
+
+}
+
+a{
+    font-size: 18px;
+    font-weight: 600;
+    color: #42A4EF;
+    margin-bottom: 10px;
+
+}
+
+h1{
+    font-size: 36px;
+    font-weight: 500;
+    color: #000000;
+
+}
+
+
+`
+
+
+export const Line = styled.div`
+
+
+`
+export const ContainerProfileCard = styled.div`
+margin-top: 35px;
+
+
+`
+export const ContainerJobCard = styled.div`
+display: flex;
+flex-wrap: wrap;
+gap: 30px;
+
+
+`
+
+
+
+
