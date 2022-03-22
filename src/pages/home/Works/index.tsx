526140d8a2550a79b47b147f715fae67720e2b24
lobo4321 <jglobo47@gmail.com>
2022-03-22 16:31:49 -0300

    fix: merge errors

--- src/pages/home/Works/index.tsx
+++ src/pages/home/Works/index.tsx
@@ -7,11 +7,8 @@ import { useModal } from "../../../hooks/useModal";
 import { ModalAlright } from "../../components/modal/ModalAlright";
 import { ModalApproved } from "../../components/modal/ModalApproved";
 import { ModalDealDone } from "../../components/modal/ModalDealDone";
-<<<<<<< HEAD
 import { ModalHelp } from "../../components/modal/ModalHelp";
-=======
 import { ModalNotApproved } from "../../components/modal/ModalNotApproved";
->>>>>>> 049e3c159a23c3fc79b82294545a199588247d56
 import { ModalRules } from "../../components/modal/ModalRules";
 import { ModalWontFinish } from "../../components/modal/ModalWontFinish";
 
@@ -186,13 +183,8 @@ export function Works() {
         <Modal
           isShown={isShown}
           hide={toggle}
-<<<<<<< HEAD
-          headerText="Banana 123"
-          modalContent={<ModalDealDone />}
-=======
           headerText="Trabalho Aprovado"
-          modalContent={<ModalNotApproved/>}
->>>>>>> 049e3c159a23c3fc79b82294545a199588247d56
+          modalContent={<ModalNotApproved />}
         />
       </S.ButtonContainer>
     </S.Wrapper>
