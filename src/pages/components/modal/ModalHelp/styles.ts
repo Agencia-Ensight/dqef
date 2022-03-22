fd9ea2fc5e9174495b32198819268d9763b65919
lobo4321 <jglobo47@gmail.com>
2022-03-22 16:10:41 -0300

    feat: update card

---
+++ src/pages/components/modal/ModalHelp/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const Button = styled.button`
+    width: 80%;
+    color: black;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    border: 3px solid #42A4EF;
+    border-radius: 99px;
+    padding: 10px 30px;
+
+`
+
+export const Description = styled.h3`
+    color: #4E4E4E;
+    font-size: 1.5rem;
+    text-align: center;
+`
+
+export const Span = styled.a`
+    text-decoration: underline;
+`
