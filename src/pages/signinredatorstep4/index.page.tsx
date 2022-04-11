6b707afd6969a880754e5d76a37fb2b1779ec94a
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-04-10 21:51:56 -0300

    Finish flow initial for registration

--- src/pages/signinredatorstep4/index.page.tsx
+++ src/pages/signinredatorstep4/index.page.tsx
@@ -3,19 +3,25 @@ import * as S from "./styles";
 import { Checkbox, CheckboxGroup } from '@chakra-ui/react';
 import { AiOutlineReload } from "react-icons/ai";
 import { Input } from "../../components/Input";
-import { ISignUpData, signUp } from "../../services/auth";
+import { ISignUpData, IValidateCodeData, signUp, validateCode } from "../../services/auth";
 import React from "react";
+import Router from "next/router";
 
 
 export default function signinredatorstep4() {
   const [emailCode, setEmailCode] = React.useState("");
 
-  const resendCode = async () => {
+  const getSignupData = () => {
     const existingData = sessionStorage.getItem('signupData');
     if (existingData) {
+      return JSON.parse(existingData);
+    }
+    return null;
+  };
 
-      const data = JSON.parse(existingData);
-      // Prepare data
+  const resendCode = async () => {
+    const data = getSignupData();
+    if (data) {
       const payload: ISignUpData = {
         email: data.email,
         password: data.password,
@@ -32,6 +38,33 @@ export default function signinredatorstep4() {
     }
   }
 
+  const validateEmailCode = async () => {
+    const data = getSignupData();
+    if (data) {
+      const payload: IValidateCodeData = {
+        email: data.email,
+        code: emailCode,
+        password: data.password,
+      }
+      return await validateCode(payload);
+    }
+  }
+
+  function handleError(error: any) {
+    // TODO: implement error handling
+    console.error(error);
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await validateEmailCode();
+      Router.push("/signinredatorstep5");
+    } catch (error) {
+      handleError(error);
+    }
+  }
+
 
   return (
     <S.Wrapper>
@@ -39,20 +72,22 @@ export default function signinredatorstep4() {
         <S.Image src="/images/signinredator.png" />
       </S.ContainerImage>
 
-      <S.ContainerInformation>
-        <a href="#">Voltar</a>
-        <h1>Inserir Código</h1>
-        <p>Insira o código que você recebeu no e-mail.</p>
-        <div>
-          <Input label="E-mail" placeholder="Insira o seu e-mail" value={emailCode} onChange={(e) => setEmailCode(e.target.value)} />
-        </div>
+      <form onSubmit={handleSubmit}>
+        <S.ContainerInformation>
+          <a href="#">Voltar</a>
+          <h1>Inserir Código</h1>
+          <p>Insira o código que você recebeu no e-mail.</p>
+          <div>
+            <Input label="E-mail" placeholder="Insira o seu e-mail" value={emailCode} onChange={(e) => setEmailCode(e.target.value)} />
+          </div>
 
-        <S.ContainerInformationDosBang onClick={resendCode}>
-          <AiOutlineReload color="var(--blue)" />
-          <h2>Enviar Código Novamente</h2>
-        </S.ContainerInformationDosBang>
-        <ButtonKnewave variant="PRIMARY" size="sm" >Criar Conta</ButtonKnewave>
-      </S.ContainerInformation>
+          <S.ContainerInformationDosBang onClick={resendCode}>
+            <AiOutlineReload color="var(--blue)" />
+            <h2>Enviar Código Novamente</h2>
+          </S.ContainerInformationDosBang>
+          <ButtonKnewave variant="PRIMARY" size="sm" type="submit">Criar Conta</ButtonKnewave>
+        </S.ContainerInformation>
+      </form>
 
 
 
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,17 +1,27 @@
 import { apiClient } from './api';
 
 export interface ISignUpData {
-    email: string;
-	password: string;
-	name: string;
-	phone: string;
-	cpf: string;
-	type: number;
-	formation: number;
-	course: number;
-	college: number;
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+  cpf: string;
+  type: number;
+  formation: number;
+  course: number;
+  college: number;
 }
 export const signUp = async (data: ISignUpData) => {
-    const response = await apiClient.post('/auth/sign-up', data);
-    return response.data;
+  const response = await apiClient.post('/auth/sign-up', data);
+  return response.data;
+}
+
+export interface IValidateCodeData {
+  email: string;
+  code: string;
+  password: string;
+}
+export const validateCode = async (data: IValidateCodeData) => {
+  const response = await apiClient.post('/auth/sign-up/code', data);
+  return response.data;
 }
