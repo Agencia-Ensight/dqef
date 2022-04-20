8e6f2446c1fce61f943d45d9f0829b42a1a0400b
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-04-19 22:13:00 -0300

    Add job listing

--- src/pages/home/Works/index.tsx
+++ src/pages/home/Works/index.tsx
@@ -1,7 +1,35 @@
+import { gql, useQuery } from "@apollo/client";
 import { ButtonKnewave } from "../../../components/ButtonKnewave";
 import { UrgentWorkCard } from "../../../components/UrgentWorkCard";
+import { JobsData, JOBS_QUERY } from "../../../queries/jobs";
 
 import * as S from "./styles";
+
+const Jobs = (): JSX.Element => {
+  const { loading, error, data } = useQuery<JobsData>(JOBS_QUERY);
+
+
+  if (loading) return <p>Loading...</p >;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return <>
+    {
+      data!.jobs.map(({ value_pay, title, theme, job_type: { name: job_type_name }, higher_course: { name: higher_course_name }, delivery, job_has_knowledges }) => (
+        <UrgentWorkCard
+          course="ECONOMIA" // higher_course_name TODO: Sincronizar com o banco de dados?
+          date={delivery}
+          discipline={job_has_knowledges.map(({ knowledge: { name: knowledge_name } }) => knowledge_name).join(", ")}
+          price={value_pay}
+          theme={theme}
+          title={title}
+          typeOfWork={job_type_name}
+          urgent={false} // TODO: Como pegar do banco de dados
+          status="EMPLOYEE-SEE" // TODO: Sincronizar com o banco de dados?
+        />
+      ))
+    }
+  </>
+}
 
 export function Works() {
   return (
@@ -15,17 +43,7 @@ export function Works() {
         </S.Description>
       </S.HeaderContainer>
       <S.MainContainer>
-        <UrgentWorkCard
-          course="ECONOMIA"
-          date="23/04 às 17h"
-          discipline="arroz"
-          price={20}
-          theme="banana"
-          title="Arroz"
-          typeOfWork="Banana"
-          urgent={false}
-          status="EMPLOYEE-SEE"
-        />
+        <Jobs />
       </S.MainContainer>
       <S.ButtonContainer>
         <ButtonKnewave size="lg" variant="PRIMARY">
---
+++ src/queries/jobs.ts
@@ -0,0 +1,57 @@
+import { gql } from "@apollo/client";
+
+export type GenericData<T, U extends string> = {
+  [_ in U]: T[];
+};
+
+export type Job = {
+  id: number;
+  date_limit: string
+  higher_course: {
+    name: string
+  },
+  job_status: {
+    name: string
+  },
+  job_type: {
+    name: string
+  },
+  theme: string
+  title: string
+  value_pay: number
+  value: number
+  delivery: string,
+  job_has_knowledges: {
+    knowledge: {
+      name: string
+    }
+  }[]
+};
+export type JobsData = GenericData<Job, 'jobs'>;
+export const JOBS_QUERY = gql`
+  query JobsQuery {
+    jobs {
+      id
+      date_limit
+      delivery
+      theme
+      title
+      value_pay
+      value
+      higher_course {
+        name
+      }
+      job_status {
+        name
+      }
+      job_type {
+        name
+      }
+      job_has_knowledges {
+        knowledge {
+          name
+        }
+      }
+    }
+  }
+`;
