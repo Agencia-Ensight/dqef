537c4e30f091154e32a0b90a422e7074c96b2b1e
Eduardo Felipe Weber <eduardo-felipe-weber@hotmail.com>
2022-05-08 10:08:28 -0300

    Componentize proposal step1

--- src/pages/proposalstudentstep1/index.page.tsx
+++ src/pages/proposalstudentstep1/index.page.tsx
@@ -1,6 +1,4 @@
 import { ButtonKnewave } from "../../components/ButtonKnewave";
-import { Input } from "../../components/Input";
-import { Select } from "@chakra-ui/react";
 import { useQuery } from "@apollo/client";
 
 import Router, { useRouter } from "next/router";
@@ -9,6 +7,7 @@ import * as S from "./styles";
 import { MultiStepForm } from "../../components/MultiStepForm";
 import { HigherCoursesData, HIGHER_COURSES, Job, JobTypesData, JOB_TYPES, KNOWLEDGES, KnowledgesData } from "../../queries/jobs";
 import { useEffect } from "react";
+import JobStep1 from "../components/jobs/JobSteps/step1";
 
 export default function proposalstudentstep1(job?: Job) {
   const router = useRouter();
@@ -49,97 +48,12 @@ export default function proposalstudentstep1(job?: Job) {
           </S.HeaderContainer>
 
           <p>Para publicar seu trabalho, insira as seguintes informações.</p>
-          <S.InputFields>
-            <S.FirstInputContainer>
-              <Input
-                label="Título do trabalho"
-                name="title"
-                placeholder="Insira o título"
-                defaultValue={job?.title}
-                mandatory={true}
-                required
-              />
-              <S.SelectContainer>
-                <label>
-                  Tipo do Trabalho<span>*</span>
-                </label>
-                <Select className="padrao" placeholder="Tipo do Trabalho" name="job_type_id" defaultValue={job?.job_type?.id}>
-                  {
-                    jobTypes?.job_types.map((jobType) => (
-                      <option key={jobType.id} value={jobType.id} selected={jobType.id === job?.job_type?.id}>
-                        {jobType.name}
-                      </option>
-                    ))
-                  }
-                </Select>
-              </S.SelectContainer>
-            </S.FirstInputContainer>
-            <S.TextInputContainer>
-              <S.SelectContainer>
-                <label>
-                  Curso do Trabalho<span>*</span>
-                </label>
-                <Select className="padrao" placeholder="Curso do Trabalho" name="higher_course_id" defaultValue={job?.higher_course?.id}>
-                  {
-                    higherCourses?.higher_courses.map((higherCourse) => (
-                      <option key={higherCourse.id} value={higherCourse.id} selected={higherCourse.id === job?.higher_course?.id}>
-                        {higherCourse.name}
-                      </option>
-                    ))
-                  }
-                </Select>
-              </S.SelectContainer>
-              <S.SelectContainer>
-                <label>
-                  Disciplina do Trabalho<span>*</span>
-                </label>
-                <Select className="padrao" placeholder="Disciplina do Trabalho" name="knowledge_id">
-                  {
-                    knowledges?.knowledges.map((knowledge) => (
-                      <option key={knowledge.id} value={knowledge.id} selected={!!job?.job_has_knowledges?.find((jobKnowledge) => jobKnowledge.knowledge.id === knowledge.id)}>
-                        {knowledge.name}
-                      </option>
-                    ))
-                  }
-                </Select>
-              </S.SelectContainer>
-              <Input
-                className="type-of-work"
-                placeholder="Tema do Trabalho"
-                label="Tema do Trabalho"
-                mandatory={true}
-                required
-                name="theme"
-                defaultValue={job?.theme}
-              />
-            </S.TextInputContainer>
-            <label>
-              Instruções<span>*</span>
-            </label>
-            <textarea
-              name="instructions"
-              id="instructions"
-              rows={5}
-              placeholder="ex: Descrição do que deve ser feito no trabalho. Explique de forma objetiva e clara, tudo que o redator deverá realizar para você. Coloque as informações importantes para o melhor entendimento do trabalho."
-              defaultValue={job?.instructions}
-            ></textarea>
-            <S.LastInputs>
-              <Input
-                label="Número de páginas"
-                placeholder="Quantidade"
-                required
-                name="pages"
-                defaultValue={job?.pages}
-              />
-              <Input
-                label="Número de palavras"
-                placeholder="Quantidade"
-                required
-                name="words"
-                defaultValue={job?.words}
-              />
-            </S.LastInputs>
-          </S.InputFields>
+          <JobStep1 
+            job={job}
+            jobTypes={jobTypes ?? { job_types: [] }}
+            higherCourses={higherCourses ?? { higher_courses: [] }}
+            knowledges={knowledges ?? { knowledges: [] }}
+          />
           <ButtonKnewave variant="PRIMARY" type="submit" size="sm">
             Próximo
           </ButtonKnewave>
--- src/pages/proposalstudentstep1/styles.ts
+++ src/pages/proposalstudentstep1/styles.ts
@@ -109,44 +109,6 @@ export const ContainerInformation = styled.div`
   }
 `;
 
-export const InputFields = styled.div`
-  margin-bottom: 1.5rem;
-  margin-top: 2.8rem;
-`;
-
-export const FirstInputContainer = styled.div`
-  display: flex;
-  gap: 100px;
-  flex-wrap: wrap;
-  margin-bottom: 30px;
-
-  @media (max-width: 600px) {
-    gap: 20px;
-  }
-`;
-export const TextInputContainer = styled.div`
-  display: flex;
-  gap: 20px;
-  margin-bottom: 30px;
-  flex-wrap: wrap;
-
-  .type-of-work {
-    width: 259px;
-
-    @media (max-width: 600px) {
-      width: 100%;
-    }
-  }
-`;
-
-export const SecondInputContainer = styled.div`
-  width: 100%;
-  gap: 20px;
-
-  display: flex;
-  gap: 50px;
-`;
-
 export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
@@ -171,20 +133,4 @@ export const Steps = styled.h5`
   @media (max-width: 600px) {
     display: none;
   }
-`;
-
-export const SelectContainer = styled.div`
-  display: inline-grid;
-
-  @media (max-width: 600px) {
-    width: 100%;
-  }
-`;
-
-export const LastInputs = styled.div`
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 20px;
-  margin-top: 30px;
 `;
